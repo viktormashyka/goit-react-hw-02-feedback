add3a49e64b8774db4f015ead9f4118f0ab41b71
Viktor Mashyka <99292051+viktormashyka@users.noreply.github.com>
2022-10-17 23:36:00 +0100

    Finished task

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export class Feedback extends Component {
-  static defaultProps = {
-    step: 1,
-  };
+  // static defaultProps = {
+  //   step: 1,
+  // };
 
   static propTypes = {};
 
@@ -12,45 +12,87 @@ export class Feedback extends Component {
     good: 0,
     neutral: 0,
     bad: 0,
+    total: 0,
+    positivePercentage: 0,
   };
 
-  countTotalFeedback = evt => {
-    console.log('countTotalFeedback');
+  // countGoodFeedback = evt => {
+  //   this.setState((states, props) => ({ good: this.state.good + 1 }));
+  // };
+
+  countGoodFeedback = async evt => {
+    await this.setState({ good: this.state.good + 1 });
+    await this.countTotalFeedback();
+    await this.countPositiveFeedbackPercentage();
   };
-  countPositiveFeedbackPercentage() {
-    console.log('countPositiveFeedbackPercentage');
-  }
+
+  countNeutralFeedback = async evt => {
+    await this.setState({ neutral: this.state.neutral + 1 });
+    await this.countTotalFeedback();
+    await this.countPositiveFeedbackPercentage();
+  };
+
+  countBadFeedback = async evt => {
+    await this.setState({ bad: this.state.bad + 1 });
+    await this.countTotalFeedback();
+    await this.countPositiveFeedbackPercentage();
+  };
+
+  countTotalFeedback = state => {
+    this.setState({
+      total: this.state.good + this.state.neutral + this.state.bad,
+    });
+  };
+
+  countPositiveFeedbackPercentage = state => {
+    this.setState({
+      positivePercentage: Math.floor(
+        (this.state.good / this.state.total) * 100
+      ),
+    });
+  };
 
   render() {
-    const { step } = this.props;
+    // const { step } = this.props;
+    const { good, neutral, bad, total, positivePercentage } = this.state;
     return (
       <div>
         <div>
           <h1>Please leave feedback</h1>
-          <button type="button" onClick={this.countTotalFeedback}>
-            Good {step}
+          <button type="button" onClick={this.countGoodFeedback}>
+            Good
           </button>
-          <button type="button" onClick={event => console.log(event)}>
+          <button type="button" onClick={this.countNeutralFeedback}>
             Neutral
           </button>
-          <button type="button" onClick={event => console.log(event)}>
+          <button type="button" onClick={this.countBadFeedback}>
             Bad
           </button>
         </div>
 
         <div>
           <h2>Statistics</h2>
-          <ul>
-            <li>Good</li>
-            <li>Neutral</li>
-            <li>Bad</li>
-            <li>Total</li>
-            <li>Positive feedback</li>
-          </ul>
+          {total > 0 && (
+            <ul>
+              <li>Good: {good}</li>
+              <li>Neutral: {neutral}</li>
+              <li>Bad: {bad}</li>
+              <li>Total: {total}</li>
+              <li>Positive feedback: {positivePercentage}%</li>
+            </ul>
+          )}
         </div>
       </div>
     );
   }
 }
 
-Feedback.propTypes = {};
+Feedback.propTypes = {
+  state: PropTypes.shape({
+    good: PropTypes.number.isRequired,
+    neutral: PropTypes.number.isRequired,
+    bad: PropTypes.number.isRequired,
+    total: PropTypes.number.isRequired,
+    positivePercentage: PropTypes.number.isRequired,
+  }).isRequired,
+};
